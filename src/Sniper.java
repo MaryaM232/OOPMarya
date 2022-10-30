b4b21f7c911cf0783d7cd5c3c44185e1ad1b9a23
MaryaM232 <mamonovamariya@mail.ru>
2022-10-30 22:42:37 +0300

    new code Sniper.java

--- src/Sniper.java
+++ src/Sniper.java
@@ -1,7 +1,6 @@
 public class Sniper extends BaseHero {
     public Sniper() {
-        super(String.format("Sniper#%d", ++Sniper.number),
-                15, 12, 10, 32, new int[]{8, 10}, 9);
+        super(String.format("Sniper#%d", ++Sniper.number), 15, 12, 10, 32, new int[]{8, 10}, 9);
     }
 }
 
